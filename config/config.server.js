8736c1bc2a2592916e446b29d482b924a195c477
bidyut <bidyut.sahoo73@gmail.com>
2022-09-04 23:02:15 +0530

    just test ecommerce branch in application

--- config/config.server.js
+++ config/config.server.js
@@ -1,11 +1,11 @@
-//let serverPort = 4500;
+    //let serverPort = 4500;
 
-//if(process.env.NODE_ENV !== 'production'){
+     if(process.env.NODE_ENV !== 'production'){
    
- //if(process.env.NODE_ENV === 'devlopment'){
-    if(process.env.NODE_ENV === 'production'){      ///eta use kora holo karon upore ja server port ache seta ke output korara jonno and .env file ja devlopment ache sater jonno
-    require('dotenv').config()
-   // serverPort =  process.env.PORT
+     //if(process.env.NODE_ENV === 'devlopment'){
+ //if(process.env.NODE_ENV === 'production'){      ///eta use kora holo karon upore ja server port ache seta ke output korara jonno and .env file ja devlopment ache sater jonno
+      require('dotenv').config()
+      //serverPort =  process.env.PORT
 }
 
 module.exports ={
